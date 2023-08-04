53cf5e8ff733bfc23816cccf7938bcdd0ecd25db
Meir <meirtubiana@gmail.com>
2023-08-04 04:40:33 +0300

    Component structuring for uploads

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          <div className="flex text-neutral-800 bg-neutral-100 min-h-screen justify-center overflow-hidden">
+          <div className="flex text-neutral-800 background-gradient min-h-screen justify-center overflow-hidden">
             <div className="py-12 flex flex-col justify-between w-7/12">
               <Navbar />
               <main className="h-full py-16">{children}</main>
--- src/components/private/explore/Trending.jsx
+++ src/components/private/explore/Trending.jsx
@@ -1,3 +1,4 @@
+import UploadPrimary from "@/components/public/uploads/UploadPrimary";
 import { getIcons } from "@/services/icons";
 import moment from "moment";
 import Image from "next/image";
@@ -12,48 +13,7 @@ const Trending = ({ data }) => {
       <hr />
       <div className="grid grid-cols-3 gap-8">
         {data.map((item) => (
-          <Link
-            href={`/explore/${item._id}`}
-            key={item._id}
-            className="bg-white w-full border cursor-pointer relative hover:bg-neutral-50 transition border-neutral-400/50 h-full flex flex-col rounded-xl p-4 gap-3"
-          >
-            <span className="absolute -top-2 text-2xl -left-2">
-              {getIcons(item.type)}
-            </span>
-            <div className="flex gap-2 items-center">
-              <h4>{item.title}</h4>
-            </div>
-            <div className="flex gap-2 items-center">
-              <Image
-                alt={item.author.username}
-                width={30}
-                height={30}
-                className="object-fit aspect-square rounded-full"
-                src={item.author.avatar}
-              />
-              <span className="text-xs text-neutral-500">
-                @<strong className="text-sm">{item.author.username}</strong>
-              </span>
-            </div>
-            <hr />
-            <div className="flex justify-between text-xs items-end text-neutral-500 justify-self-end h-full">
-              <div className="flex gap-3 items-center">
-                <span className="flex gap-1 items-center">
-                  <span>
-                    {parseInt(item.comments?.length || 0).toLocaleString()}
-                  </span>{" "}
-                  <BsFillChatDotsFill className="text-blue-600" />
-                </span>
-                <span className="flex gap-1 items-center">
-                  <span>
-                    {parseInt(item.likes?.length || 0).toLocaleString()}
-                  </span>
-                  <FcLike />
-                </span>
-              </div>
-              <span>{moment(item.createdAt).startOf("hour").fromNow()}</span>
-            </div>
-          </Link>
+          <UploadPrimary item={item} />
         ))}
       </div>
     </section>
